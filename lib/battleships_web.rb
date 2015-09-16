318263172833385ead294cc5eafef0aae8c26ff8
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-09-16 12:06:34 +0100

    version 1 almost complete

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,9 +2,19 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/new_game' do
+    @visitor = params[:name]
+    erb :new_game
+  end
+
+  get '/start_game' do
+    erb :start_game
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+  set :views, proc { File.join(root, '..', 'views') }
 end
